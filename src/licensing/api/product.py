4d921517c08e32be3e463853bb3302cb16359a6e
Rupert Schachinger <rupert.schachinger@bettermarks.com>
2023-02-08 21:07:34 +0100

    proper 404s on not found products

--- src/licensing/api/product.py
+++ src/licensing/api/product.py
@@ -1,7 +1,7 @@
-from fastapi import APIRouter, Depends
-from typing import List
-
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi import status as http_status
 from sqlalchemy.ext.asyncio import AsyncSession
+from typing import List
 
 from licensing.db import get_async_session
 from licensing.schema import product as schema
@@ -10,13 +10,17 @@ from licensing.crud import product as crud
 router = APIRouter()
 
 
-@router.get("/", response_model=List[schema.ProductGet])
+@router.get("/", response_model=List[schema.ProductGet], status_code=http_status.HTTP_200_OK)
 async def get_products(session: AsyncSession = Depends(get_async_session)):
     return await crud.get_products(session)
 
 
-@router.get("/{product_id}", response_model=schema.ProductGet)
+@router.get("/{product_id}", response_model=schema.ProductGet, status_code=http_status.HTTP_200_OK)
 async def get_product(product_id, session: AsyncSession = Depends(get_async_session)):
-    return await crud.get_product(session, product_id)
-
-
+    product = await crud.get_product(session, product_id)
+    if product is None:
+        raise HTTPException(
+            status_code=http_status.HTTP_404_NOT_FOUND,
+            detail="The product could not be found."
+        )
+    return product
--- src/licensing/crud/product.py
+++ src/licensing/crud/product.py
@@ -2,13 +2,19 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from licensing.model import product as model
+from licensing.schema.product import ProductGet
 
 
 async def get_products(session: AsyncSession):
     return (await session.execute(select(model.Product))).scalars().all()
 
 
-async def get_product(session: AsyncSession, eid: str):
-    return (await session.execute(select(model.Product).where(model.Product.eid == eid))).scalar()
+async def get_product(session: AsyncSession, eid: str) -> ProductGet:
+    statement = select(
+        model.Product
+    ).where(
+        model.Product.eid == eid
+    )
+    return (await session.execute(statement=statement)).scalar_one_or_none()
 
 
