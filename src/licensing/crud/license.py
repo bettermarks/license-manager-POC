5881561f37fce8c0ca3d4c4cb2f23659a40ac9f0
Rupert Schachinger <rupert.schachinger@bettermarks.com>
2023-02-09 12:28:16 +0100

    license purchase

--- src/licensing/crud/license.py
+++ src/licensing/crud/license.py
@@ -3,24 +3,34 @@ from fastapi import status as http_status, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from licensing.crud.product import get_product
-from licensing.model import license as model
-from licensing.schema import license as schema
+from licensing.model import product as product_model
+from licensing.model import license as license_model
+from licensing.schema import license as license_schema
 
 
-async def purchase_license(
-        session: AsyncSession, purchaser_eid: str, license_data: schema.LicenseCreate
-) -> model.License:
-
-    # 1. find product
-    product = await get_product(session, license_data.product_eid)
+async def find_product(session: AsyncSession, product_eid: str) -> product_model.Product:
+    """finds a product by a given product EID or raises an HTTPException"""
+    product = await get_product(session, product_eid)
     if not product:
         raise HTTPException(
             status_code=http_status.HTTP_400_BAD_REQUEST,
-            detail=f"Product with EID='{license_data.product_eid}' not found. License purchase failed."
+            detail=f"Product with EID='{product_eid}' not found. License purchase failed."
         )
+    return product
+
+
+async def purchase_license(
+        session: AsyncSession, purchaser_eid: str, license_data: license_schema.LicenseCreate
+) -> license_model.License:
+
+    # 0. check, if requesting user is purchaser
+    # TODO
+
+    # 1. find product
+    product = await find_product(session, license_data.product_eid)
 
     # 2. create license
-    lic = model.License(
+    lic = license_model.License(
         ref_product=product.id,
         purchaser_eid=purchaser_eid,
         **{k: v for k, v in license_data if k != "product_eid"}
--- src/licensing/crud/product.py
+++ src/licensing/crud/product.py
@@ -2,27 +2,27 @@ from typing import List
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from licensing.model import product as model
-from licensing.schema import product as schema
+from licensing.model import product as product_model
+from licensing.schema import product as product_schema
 
 
-async def get_products(session: AsyncSession) -> List[schema.Product]:
+async def get_products(session: AsyncSession) -> List[product_schema.Product]:
     return (
         await session.execute(
             statement=select(
-                model.Product
+                product_model.Product
             )
         )
     ).scalars().all()
 
 
-async def get_product(session: AsyncSession, eid: str) -> schema.Product:
+async def get_product(session: AsyncSession, eid: str) -> product_schema.Product:
     return (
         await session.execute(
             statement=select(
-                model.Product
+                product_model.Product
             ).where(
-                model.Product.eid == eid
+                product_model.Product.eid == eid
             )
         )
     ).scalar_one_or_none()
