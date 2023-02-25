d4f6c27c48b6d161127c8f4fbf357358dc933088
Rupert Schachinger <rupert.schachinger@bettermarks.com>
2023-02-25 21:30:26 +0100

    more tests

--- tests/integration/test_product.py
+++ tests/integration/test_product.py
@@ -12,14 +12,14 @@ from tests.integration.initial_data import INITIAL_TEST_PRODUCTS
 
 
 @pytest.mark.asyncio
-async def test_get_products(async_test_client: AsyncClient):
+async def test_get_products__ok(async_test_client: AsyncClient):
     response = await async_test_client.get("/products/")
     assert response.status_code == 200
     assert json.loads(response._content) == [asdict(schema.Product.from_orm(p)) for p in INITIAL_TEST_PRODUCTS]
 
 
 @pytest.mark.asyncio
-async def test_get_product(async_test_client: AsyncClient, async_test_session: AsyncSession):
+async def test_get_product__ok(async_test_client: AsyncClient, async_test_session: AsyncSession):
     eid = "full_access"
     response = await async_test_client.get(f"/products/{eid}")
     assert response.status_code == 200
@@ -27,3 +27,11 @@ async def test_get_product(async_test_client: AsyncClient, async_test_session: A
         select(model.Product).where(model.Product.eid == eid))
     ).scalar_one_or_none()
     assert json.loads(response._content) == asdict(schema.Product.from_orm(product))
+
+
+@pytest.mark.asyncio
+async def test_get_product__404(async_test_client: AsyncClient, async_test_session: AsyncSession):
+    eid = "something"
+    response = await async_test_client.get(f"/products/{eid}")
+    assert response.status_code == 404
+    assert json.loads(response._content) == {'detail': 'The product could not be found.'}
